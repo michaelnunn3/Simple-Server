4c7d7d575ac632cd5539c856bf079a7c0da8c7a8
michaelnunn3 <michaelnunn@ufl.edu>
2020-02-12 22:09:57 -0500

    Working on disconnect

--- src/client.java
+++ src/client.java
@@ -3,32 +3,62 @@ import java.io.*;
 
 class client
 {
-    public static void main(String args[]) throws IOException
-    {
-        String message;
-        Socket client = new Socket("localhost", 3359);
-        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        message = br.readLine();
-        System.out.println(message);
 
-        DataOutputStream dos = new DataOutputStream(client.getOutputStream());
-        
-        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
-
-        String toServer, fromServer;
-
-        while (!(toServer = kb.readLine()).equals("exit")) {
-            dos.writeBytes(toServer + "\n");
-
-            fromServer = br.readLine();
+    // Method handles data recieved from server
+    public static void displayData(String fromServer) {
+        if (Integer.parseInt(fromServer) > 0) {
             System.out.println(fromServer);
         }
+        else if (Integer.parseInt(fromServer) == -1) {
+            System.out.println("ERROR -1: incorrect operation command.");
+        }
+        else if (Integer.parseInt(fromServer) == -2) {
+            System.out.println("ERROR -2: number of inputs is less than two.");
+        }
+        else if (Integer.parseInt(fromServer) == -3) {
+            System.out.println("ERROR -3: number of inputs is more than four.");
+        }
+        else if (Integer.parseInt(fromServer) == -4) {
+            System.out.println("ERROR -4: one or more of the inputs contain(s) non-number(s).");
+        }
+        else if (Integer.parseInt(fromServer) == -5) {
+            System.out.println("-5: exit");
 
-        // close connection. 
+        }
+    }
+
+    public static void main(String args[]) throws IOException
+    {
+        // Create client socket
+        Socket client = new Socket(args[0], Integer.parseInt(args[1]));
+
+        // Create BufferedReader br, DataOutputStream dos and BufferedReader kb
+        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+
+        // Reading connection message
+        System.out.println(br.readLine());
+
+        // Declaring strings
+        String toServer, fromServer;
+
+        // While connection is established
+        while (!(toServer = kb.readLine()).equals(null)) {
+            // Writing to server
+            dos.writeBytes(toServer + "\n");
+
+            // Displaying data from server
+            fromServer = br.readLine();
+            displayData(fromServer);
+            if (Integer.parseInt(fromServer) == -5) {
+                break;
+            }
+        }
+        // Close client connection
         dos.close(); 
         br.close(); 
         kb.close(); 
         client.close(); 
-
     }
 }
--- src/server.java
+++ src/server.java
@@ -3,69 +3,108 @@ import java.io.*;
 
 class server
 {
+
+    // Method completes operations from client input and returns
+    public static int operations(String[] words) {
+        int answer = Integer.parseInt(words[1]);
+
+            if (words[0].equals("add")) {
+                for(int i = 2; i < words.length; i++) {
+                    answer = answer + Integer.parseInt(words[i]);
+                }
+            }
+            else if (words[0].equals("subtract")) {
+                for(int i = 2; i < words.length; i++) {
+                    answer = answer - Integer.parseInt(words[i]);
+                }
+            }
+            else if (words[0].equals("multiply")) {
+                for(int i = 2; i < words.length; i++) {
+                    answer = answer*Integer.parseInt(words[i]);
+                }
+            }
+
+        return answer;
+    }
+
+    // Method to check if array only contains ints
+    public static boolean areAllInts(String[] words) {
+        for (int i = 1; i < words.length; i++) {
+            try {
+                Integer.parseInt(words[i]);
+            }
+            catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Method will handle errors or send back correct answer to client
+    public static void runWithErrorHandler(PrintStream ps, BufferedReader br, ServerSocket server, Socket client, String[] words) throws IOException {
+        if (!(words[0].equals("add") || words[0].equals("subtract") || words[0].equals("multiply") || words[0].equals("bye") || words[0].equals("terminate"))) {
+            ps.println(-1);
+        } 
+        else if (words[0].equals("bye")) {
+            ps.println(-5);
+        }
+        else if (words[0].equals("terminate")) {
+            ps.println(-5);
+        }
+        else if (words.length <= 2) {
+            ps.println(-2);
+        }
+        else if (words.length > 5) {
+            ps.println(-3);
+        }
+        else if (!areAllInts(words)) {
+            ps.println(-4);
+        }
+        else {
+            int answer = operations(words);
+            ps.println(answer);
+        }
+    }
+
     public static void main(String args[]) throws IOException
     {
-        ServerSocket server = new ServerSocket (3359);
-        System.out.println("Server waiting for client connection on port 3359");
-
+        // Create server socket and establish connection
+        ServerSocket server = new ServerSocket (Integer.parseInt(args[0]));
+        System.out.println("Server waiting for client connection on port " + args[0]);
         Socket client = server.accept();
         System.out.println("Connection Established");
 
+        // Create PrintStream ps and BufferedReader br
         PrintStream ps = new PrintStream(client.getOutputStream());
         BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
+        // Send connection message to client
         ps.println("Hello!");
 
+        // Declare string
+        String fromClient;
+        
+        // While connection is established
         while(true) {
-            
-            String fromClient, toClient;
-
             while((fromClient = br.readLine()) != null) {
+                // Creating string array to parse
                 String[] words = fromClient.split("\\W+");
 
-                if (!(words[0].equals("add") || words[0].equals("subtract") || words[0].equals("multiply"))) {
-                    ps.println("ERROR -1: incorrect operation command.");
-                } 
-                else if (words.length <= 1) {
-                    ps.println("ERROR -2: number of inputs is less than two.");
-                }
-                else if (words.length > 4) {
-                    ps.println("ERROR -3: number of inputs is more than four.");
-                }
-                else {
-                    int answer = Integer.parseInt(words[1]);
+                // Running code
+                runWithErrorHandler(ps, br, server, client, words);
+            }   
 
-                    if (words[0].equals("add")) {
-                        for(int i = 2; i < words.length; i++) {
-                            answer = answer + Integer.parseInt(words[i]);
-                        }
-                    }
-                    else if (words[0].equals("subtract")) {
-                        for(int i = 2; i < words.length; i++) {
-                            answer = answer - Integer.parseInt(words[i]);
-                        }
-                    }
-                    else if (words[0].equals("multiply")) {
-                        for(int i = 2; i < words.length; i++) {
-                            answer = answer*Integer.parseInt(words[i]);
-                        }
-                    }
-
-                    ps.println("Answer: " + answer);
-
-                    }
-
-            }
-
-            // close connection 
+            // Close connection
             ps.close(); 
-            br.close();  
-            server.close(); 
-            client.close(); 
-  
-            // terminate application 
+            //br.close();  
+            //server.close(); 
+            client.close();
+            System.out.println("Client Disconnected");
+            break;
+
+            // Terminate application 
             //System.exit(0); 
-                
-        }
+
+        }   
     }
 }
